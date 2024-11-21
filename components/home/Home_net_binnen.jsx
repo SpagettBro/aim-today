3c0b83b95aad6864e578f436c87c35cb29ae5880
BoHofland <bo.hofland@student.fontys.nl>
2024-11-21 10:14:53 +0100

    hero sectie gemaakt + begin blok_container

--- components/home/Home_net_binnen.jsx
+++ components/home/Home_net_binnen.jsx
@@ -29,6 +29,47 @@ const Home_net_binnen = ({ blok }) => (
     <div className="hoofdartikel_titel font-dm-sans text-xl font-black pt-2 ml-2 leading-5">
       {render(blok.hoofdartikel_titel)}
     </div>
+
+    <div className="mt-8 px-4">
+      <div className="relative w-full max-w-[350px] mx-auto mt-4 flex gap-4">
+              <div className="absolute inset-0 rounded-md transform translate-x-2 translate-y-2"></div>
+        {/*Linker afbeelding*/}
+        <div className="w-1/2">
+         <Image
+          src={blok.netBinnen_img_links.filename}
+          alt={blok.netBinnen_img_links.alt || "afbeelding artikel links"}
+          width={350} 
+          height={215} 
+          className="relative w- h-auto" 
+       />
+        </div>
+
+        {/*Linker tekst*/}
+        <div className="w-1/2">
+        <p className="font-montserrat text-xs font-normal pt-5 ml-2">{blok.netBinnen_accent_links}</p>
+        <p className="font-dm-sans text-sm font-bold pt-2 ml-2 leading-5">{blok.netBinnen_titel_links}</p>
+        </div>
+      </div>
+
+      <div className="flex gap-4 mt-8">
+        {/*Rechter afbeelding*/}
+        <div className="w-1/2">
+        <Image
+          src={blok.netBinnen_img_rechts.filename}
+          alt={blok.netBinnen_img_rechts.alt || "afbeelding artikel rechts"}
+          width={350} 
+          height={215} 
+          className="relative w- h-auto"
+        />
+        </div>
+
+        {/*Rechter tekst*/}
+        <div className="w-1/2">
+        <p className="font-montserrat text-xs font-normal pt-5 ml-2">{blok.netBinnen_accent_rechts}</p>
+        <p className="font-dm-sans text-sm font-bold pt-2 ml-2 leading-5">{blok.netBinnen_titel_rechts}</p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
