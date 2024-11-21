e2ddb070b4d4958ab63f77cf41ace3e776ab287f
Teunert <teunert@users.noreply.github.com>
2024-11-21 09:47:42 +0100

    update navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = ({ blok }) => {
       {...storyblokEditable(blok)}
     >
       <div className="flex items-center justify-between max-w-7xl mx-auto py-3">
-        {/* Logo on the far left */}
         <div className="flex-shrink-0">
           <img
             src={blok.logo.filename}
@@ -23,33 +22,31 @@ const Navbar = ({ blok }) => {
           />
         </div>
 
-        {/* Navigation and Buttons with Padding */}
         <div className="flex items-center justify-between flex-grow ml-[30px]">
-          {/* Navigation */}
           <nav className="hidden lg:flex space-x-6">
             <a
-              href="/world"
+              href="/nieuws"
               className="text-gray-500 group hover:text-black transition duration-300 px-[10px]"
             >
               Nieuws
               <span className="block w-full h-[2px] bg-orange-500 opacity-0 group-hover:opacity-100 transition-all"></span>
             </a>
             <a
-              href="/politics"
+              href="/interviews"
               className="text-gray-500 group hover:text-black transition duration-300 px-[10px]"
             >
               Interviews
               <span className="block w-full h-[2px] bg-orange-500 opacity-0 group-hover:opacity-100 transition-all"></span>
             </a>
             <a
-              href="/business"
+              href="/artikelen"
               className="text-gray-500 group hover:text-black transition duration-300 px-[10px]"
             >
               Artikelen
               <span className="block w-full h-[2px] bg-orange-500 opacity-0 group-hover:opacity-100 transition-all"></span>
             </a>
             <a
-              href="/tech"
+              href="/evenementen"
               className="text-gray-500 group hover:text-black transition duration-300 px-[10px]"
             >
               Evenementen
@@ -57,18 +54,16 @@ const Navbar = ({ blok }) => {
             </a>
           </nav>
 
-          {/* Subscription and Login Buttons (Hidden on Mobile) */}
           <div className="hidden lg:flex items-center space-x-4 ml-[225px]">
             <button className="text-gray-500 hover:text-black transition duration-300">
               Log In
             </button>
             <button className="text-white bg-DonkerBlauw px-[15px] py-[10px] rounded-md">
-              Word Abbonee
+              Word Abonnee
             </button>
           </div>
         </div>
 
-        {/* Hamburger Menu for Mobile */}
         <button
           className="lg:hidden p-2 text-gray-500 focus:outline-none"
           onClick={toggleMenu}
@@ -91,45 +86,36 @@ const Navbar = ({ blok }) => {
         </button>
       </div>
 
-      {/* Mobile Menu */}
       {menuOpen && (
-        <nav className="flex flex-col space-y-4 lg:hidden bg-white p-4 border-t border-gray-300">
+        <nav className="flex flex-col bg-gray-100 text-left p-6 space-y-4 shadow-md border-t border-gray-300 w-full lg:hidden">
           <a
-            href="/"
-            className="text-gray-500 hover:text-black hover:underline hover:text-orange-500 transition duration-300"
+            href="/nieuws"
+            className="text-gray-700 text-2xl hover:text-orange-500 transition-all flex justify-between items-center border-b border-gray-300 pb-6"
           >
-            Home
+            Nieuws
+            <span className="text-black font-bold text-xl">+</span>
           </a>
           <a
-            href="/world"
-            className="text-gray-500 hover:text-black hover:underline hover:text-orange-500 transition duration-300"
+            href="/interviews"
+            className="text-gray-700 text-2xl hover:text-orange-500 transition-all border-b border-gray-300 pb-6"
           >
-            World
+            Interviews
           </a>
           <a
-            href="/politics"
-            className="text-gray-500 hover:text-black hover:underline hover:text-orange-500 transition duration-300"
+            href="/artikelen"
+            className="text-gray-700 text-2xl hover:text-orange-500 transition-all border-b border-gray-300 pb-6"
           >
-            Politics
+            Artikelen
           </a>
           <a
-            href="/business"
-            className="text-gray-500 hover:text-black hover:underline hover:text-orange-500 transition duration-300"
+            href="/evenementen"
+            className="text-gray-700 text-2xl hover:text-orange-500 transition-all border-b border-gray-300 pb-6"
           >
-            Business
+            Evenementen
           </a>
-          <a
-            href="/tech"
-            className="text-gray-500 hover:text-black hover:underline hover:text-orange-500 transition duration-300"
-          >
-            Tech
-          </a>
-          <a
-            href="/sports"
-            className="text-gray-500 hover:text-black hover:underline hover:text-orange-500 transition duration-300"
-          >
-            Sports
-          </a>
+          <button className="bg-DarkBlue text-white px-4 py-[243px] rounded-md mt-4 self-start">
+            Word abonee
+          </button>
         </nav>
       )}
     </header>
