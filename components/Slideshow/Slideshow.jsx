8b291bc5d9c3d898a26538d58aaebb48f3ea0b2e
Jesse van Luxemburg <jessevanluxemburg@gmail.com>
2024-11-25 10:49:25 +0100

    Update Slideshow.jsx

--- components/Slideshow/Slideshow.jsx
+++ components/Slideshow/Slideshow.jsx
@@ -16,26 +16,7 @@ const Slideshow = ({ blok }) => (
         <div
           key={index}
           className="min-w-[calc(100%-1rem)] md:min-w-[calc(33.333%-1rem)] flex-shrink-0 rounded-lg overflow-hidden bg-white shadow-lg snap-start"
-        >
-          {/* Afbeelding */}
-          <div className="relative">
-            <img
-              src={slide.image.filename}
-              alt={slide.image.alt || "Slide afbeelding"}
-              className="w-full h-48 object-cover"
-            />
-            <div className="absolute inset-0 border-r-[5px] border-b-[5px] border-[#E76F51]"></div>
-          </div>
-
-          {/* Informatie onder de afbeelding */}
-          <div className="p-4">
-            <p className="text-sm text-gray-600 mb-1">{slide.date}</p>
-            <h3 className="text-lg font-semibold text-gray-800 mb-2">
-              {slide.title}
-            </h3>
-            <p className="text-sm text-gray-600">{slide.description}</p>
-          </div>
-        </div>
+        ></div>
       ))}
     </div>
   </div>
