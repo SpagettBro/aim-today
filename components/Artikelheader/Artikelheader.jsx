166a31e409725ec46779d6d854e71e8cf768fa64
SpagettBro <markgonzalez02@hotmail.com>
2024-11-25 12:07:31 +0100

    Reactie margins toegevoegd.

--- components/Artikelheader/Artikelheader.jsx
+++ components/Artikelheader/Artikelheader.jsx
@@ -2,22 +2,27 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import { render } from "storyblok-rich-text-react-renderer";
 
 const Artikelheader = ({ blok }) => (
-  <div {...storyblokEditable(blok)}>
+  <div className="mx-2"{...storyblokEditable(blok)}>
     <div>
-      <div className="font-bold text-orange-600 font-montserrat">
+      <div className="font-bold text-orange-600 font-montserrat mt-4 mb-1">
         {render(blok.categorietekst)} 
       </div>
-      <div className="font-bold font-dm-sans" >
+      <div className="font-bold font-dm-sans mb-4 text-lg" >
         {render(blok.titeltekst)}
       </div>
-      <div className="flex font-extralight">
+      <div className="mt-2 mb-4 text-sm">
+        In een tijdperk van razendsnelle technologische ontwikkelingen verandert ook marketing ingrijpend. 
+        Marketingexpert Lisa van der Meer deelt haar inzichten over trends als AI, personalisatie en datagedreven strategieën. 
+        Deze innovaties bepalen hoe bedrijven inspelen op de steeds veranderende klantbehoeften.
+      </div>
+      <div className="flex justify-between font-extralight text-xs mb-2">
         {render(blok.auteurtekst)}
         {render(blok.datumtekst)}
         {render(blok.aantaltekst)}
       </div>
       <div>
-        <img
-            className="w-full"
+        <img 
+            className="w-full rounded-none"
             src={blok.artikelfoto.filename}
             alt={blok.artikelfoto.alt || "over het artikel"}
           />
--- components/StoryblokProvider.ts
+++ components/StoryblokProvider.ts
@@ -15,7 +15,7 @@ import Footer from "./Footer/Footer";
 import Home_net_binnen from "./Home/Home_net_binnen";
 import Net_binnen_artikel from "./Net_binnen_artikel/Net_binnen_artikel";
 import Inschrijven_heading from "./Inschrijven_heading/Inschrijven_heading";
-import Reacties from "./Reacties/Reacties"; 
+import ReactieSectie from "./ReactieSectie/ReactieSectie"; 
 
 const components = {
   page: Page,
@@ -28,8 +28,7 @@ const components = {
   home_net_binnen: Home_net_binnen,
   inschrijven_heading: Inschrijven_heading,
   net_binnen_artikel: Net_binnen_artikel,
-  reacties: Reacties,
-  
+  reactiesectie: ReactieSectie,
 };
 
 storyblokInit({
