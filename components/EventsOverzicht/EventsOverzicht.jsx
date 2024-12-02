81d82b559e518e985a33305f0fd06ec31ef6fd61
Jesse van Luxemburg <jessevanluxemburg@gmail.com>
2024-12-02 11:26:18 +0100

    hovers gemaakt voor events

--- components/EventsOverzicht/EventsOverzicht.jsx
+++ components/EventsOverzicht/EventsOverzicht.jsx
@@ -4,7 +4,7 @@ import { render } from "storyblok-rich-text-react-renderer";
 
 const EventsOverzicht = ({ blok }) => (
   <div
-    className="max-w-screen-md mx-auto my-8 text-[#2a3a4b]"
+    className="max-w-screen-md mx-5 md:mx-auto my-8 text-[#2a3a4b]"
     {...storyblokEditable(blok)}
   >
     {/* Heading */}
@@ -16,19 +16,18 @@ const EventsOverzicht = ({ blok }) => (
     <div className="flex flex-col gap-8">
       {blok.events.map((event, index) => (
         <div key={index} className="flex flex-col">
-          {/* Event container */}
           <div
-            className="flex flex-col md:flex-row items-start bg-white rounded-lg overflow-hidden"
+            className="flex flex-col md:flex-row items-start bg-white rounded-lg overflow-hidden group"
             {...storyblokEditable(event)}
           >
-            {/* Afbeelding links */}
+            {/* Afbeelding links*/}
             {event.image && (
-              <div className="w-full md:w-1/2 mr-4">
+              <div className="w-full md:w-1/2 mr-4 relative cursor-pointer group-hover:cursor-pointer">
                 <div className="relative rounded-lg overflow-hidden shadow-[5px_5px_0_0_#e76f51] h-[200px]">
                   <img
                     src={event.image}
                     alt={event.image.alt || "Event afbeelding"}
-                    className="w-full h-full object-cover"
+                    className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
                   />
                 </div>
               </div>
@@ -38,7 +37,7 @@ const EventsOverzicht = ({ blok }) => (
             <div className="flex flex-col justify-between p-4 md:p-6 w-full md:w-2/3">
               {/* Titel en organisator */}
               <div className="mb-4">
-                <h3 className="text-xl font-bold font-dm mb-1">
+                <h3 className="text-xl font-bold font-dm mb-1 transition-all duration-300 group-hover:text-[#e76f51] cursor-pointer group-hover:underline">
                   {event.title ? render(event.title) : "Event Titel"}
                 </h3>
                 <p className="text-md font-light">
@@ -64,8 +63,8 @@ const EventsOverzicht = ({ blok }) => (
                   </div>
                 )}
 
-                {/* Datum*/}
-                <p className="flex items-center text-[#2a3a4b] font-semibold font-dm">
+                {/* Datum */}
+                <p className="flex items-center text-[#2a3a4b] font-medium font-montserrat">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5 mr-2 text-[#2a3a4b]"
