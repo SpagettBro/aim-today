3d32a5166bb5ba80476a08a80e4809e2d621211c
Teunert <teunert@users.noreply.github.com>
2024-12-04 09:47:25 +0100

    responsive

--- components/Feature/Feature.jsx
+++ components/Feature/Feature.jsx
@@ -1,10 +1,88 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 import { render } from "storyblok-rich-text-react-renderer";
+import Image from "next/image";
 
-const Feature = ({ blok }) => (
-  <div className="blok-feature" {...storyblokEditable(blok)}>
-    {blok.name}
-  </div>
-);
+const Feature = ({ blok }) => {
+  return (
+    <div
+      className="container mx-auto px-8 relative w-full"
+      {...storyblokEditable(blok)}
+    >
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* Feature 1 */}
+        <div className="relative w-full mx-auto mt-4">
+          <div className="relative bg-white rounded-lg shadow-md overflow-hidden">
+            <Image
+              src={blok.image.filename}
+              alt={blok.image.alt || "Afbeelding artikel"}
+              width={300}
+              height={190}
+              className="w-full h-auto"
+            />
+            <div className="p-4" style={{ height: "170px", maxWidth: "310px" }}>
+              <p className="font-montserrat text-xs font-normal text-gray-500">
+                {blok.accent}
+              </p>
+              <h3 className="font-dm-sans text-sm font-bold leading-5 pt-2">
+                {blok.title}
+              </h3>
+              <p className="text-sm text-gray-600 pt-2">
+                {render(blok.description)}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Feature 2 */}
+        <div className="relative w-full mx-auto mt-4">
+          <div className="relative bg-white rounded-lg shadow-md overflow-hidden">
+            <Image
+              src={blok.image.filename}
+              alt={blok.image.alt || "Afbeelding artikel"}
+              width={300}
+              height={190}
+              className="w-full h-auto"
+            />
+            <div className="p-4" style={{ height: "170px", maxWidth: "310px" }}>
+              <p className="font-montserrat text-xs font-normal text-gray-500">
+                {blok.accent}
+              </p>
+              <h3 className="font-dm-sans text-sm font-bold leading-5 pt-2">
+                {blok.title}
+              </h3>
+              <p className="text-sm text-gray-600 pt-2">
+                {render(blok.description)}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Feature 3 */}
+        <div className="relative w-full mx-auto mt-4">
+          <div className="relative bg-white rounded-lg shadow-md overflow-hidden">
+            <Image
+              src={blok.image.filename}
+              alt={blok.image.alt || "Afbeelding artikel"}
+              width={300}
+              height={190}
+              className="w-full h-auto"
+            />
+            <div className="p-4" style={{ height: "170px", maxWidth: "310px" }}>
+              <p className="font-montserrat text-xs font-normal text-gray-500">
+                {blok.accent}
+              </p>
+              <h3 className="font-dm-sans text-sm font-bold leading-5 pt-2">
+                {blok.title}
+              </h3>
+              <p className="text-sm text-gray-600 pt-2">
+                {render(blok.description)}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Feature;
--- components/home/Home_net_binnen.jsx
+++ components/home/Home_net_binnen.jsx
@@ -1,12 +1,10 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 import { render } from "storyblok-rich-text-react-renderer";
-import Image from 'next/image'; 
+import Image from "next/image";
 <link
   href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@900&family=Montserrat:wght@400&display=swap"
   rel="stylesheet"
-/>
-
-
+/>;
 
 const Home_net_binnen = ({ blok }) => (
   <div className="relative w-full px-4" {...storyblokEditable(blok)}>
@@ -18,55 +16,65 @@ const Home_net_binnen = ({ blok }) => (
       <Image
         src={blok.home_hoofdartikel_img.filename}
         alt={blok.home_hoofdartikel_img.alt || "Hoofd artikel img"}
-        width={350} 
-        height={215} 
+        width={350}
+        height={215}
         className="relative w- h-auto" // Afbeelding breedte instellen op 100% van de container
       />
     </div>
     {/* Hoofdartikel accent met Montserrat font en 12px grootte */}
-    <p className="hoofdartikel_accent font-montserrat text-xs font-normal pt-5 ml-2">{blok.hoofdartikel_accent}</p>
- {/* Hoofdartikel titel met DM Sans font en 20px grootte */}
+    <p className="hoofdartikel_accent font-montserrat text-xs font-normal pt-5 ml-2">
+      {blok.hoofdartikel_accent}
+    </p>
+    {/* Hoofdartikel titel met DM Sans font en 20px grootte */}
     <div className="hoofdartikel_titel font-dm-sans text-xl font-black pt-2 ml-2 leading-5">
       {render(blok.hoofdartikel_titel)}
     </div>
 
     <div className="mt-8 px-4">
       <div className="relative w-full max-w-[350px] mx-auto mt-4 flex gap-4">
-              <div className="absolute inset-0 rounded-md transform translate-x-2 translate-y-2"></div>
+        <div className="absolute inset-0 rounded-md transform translate-x-2 translate-y-2"></div>
         {/*Linker afbeelding*/}
         <div className="w-1/2">
-         <Image
-          src={blok.netBinnen_img_links.filename}
-          alt={blok.netBinnen_img_links.alt || "afbeelding artikel links"}
-          width={350} 
-          height={215} 
-          className="relative w- h-auto" 
-       />
+          <Image
+            src={blok.netBinnen_img_links.filename}
+            alt={blok.netBinnen_img_links.alt || "afbeelding artikel links"}
+            width={350}
+            height={215}
+            className="relative w- h-auto"
+          />
         </div>
 
         {/*Linker tekst*/}
         <div className="w-1/2">
-        <p className="font-montserrat text-xs font-normal pt-5 ml-2">{blok.netBinnen_accent_links}</p>
-        <p className="font-dm-sans text-sm font-bold pt-2 ml-2 leading-5">{blok.netBinnen_titel_links}</p>
+          <p className="font-montserrat text-xs font-normal pt-5 ml-2">
+            {blok.netBinnen_accent_links}
+          </p>
+          <p className="font-dm-sans text-sm font-bold pt-2 ml-2 leading-5">
+            {blok.netBinnen_titel_links}
+          </p>
         </div>
       </div>
 
       <div className="flex gap-4 mt-8">
         {/*Rechter afbeelding*/}
         <div className="w-1/2">
-        <Image
-          src={blok.netBinnen_img_rechts.filename}
-          alt={blok.netBinnen_img_rechts.alt || "afbeelding artikel rechts"}
-          width={350} 
-          height={215} 
-          className="relative w- h-auto"
-        />
+          <Image
+            src={blok.netBinnen_img_rechts.filename}
+            alt={blok.netBinnen_img_rechts.alt || "afbeelding artikel rechts"}
+            width={350}
+            height={215}
+            className="relative w- h-auto"
+          />
         </div>
 
         {/*Rechter tekst*/}
         <div className="w-1/2">
-        <p className="font-montserrat text-xs font-normal pt-5 ml-2">{blok.netBinnen_accent_rechts}</p>
-        <p className="font-dm-sans text-sm font-bold pt-2 ml-2 leading-5">{blok.netBinnen_titel_rechts}</p>
+          <p className="font-montserrat text-xs font-normal pt-5 ml-2">
+            {blok.netBinnen_accent_rechts}
+          </p>
+          <p className="font-dm-sans text-sm font-bold pt-2 ml-2 leading-5">
+            {blok.netBinnen_titel_rechts}
+          </p>
         </div>
       </div>
     </div>
