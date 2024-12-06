44ea1f7bdd3193c04cfb4f45506be65ca02b313b
Lucas <lucasbitalia@gmail.com>
2024-12-06 13:30:08 +0100

    darkmode toegevoegd op een artikel

--- components/Artikelfoot/Artikelfoot.jsx
+++ components/Artikelfoot/Artikelfoot.jsx
@@ -1,9 +1,12 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 import { StoryblokComponent } from "@storyblok/react/rsc";
 
-const Artikelfoot= ({ blok }) => (
-  <div className="flex h-fit justify-between mx-4 my-8 text-sm lg:w-1/2 lg:mx-auto " {...storyblokEditable(blok)}>
-      {blok.artikelgrid?.map((nestedBlok) => (
+const Artikelfoot = ({ blok }) => (
+  <div
+    className="flex h-fit justify-between mx-4 my-8 text-sm lg:w-1/2 lg:mx-auto dark:bg-darkGrijs] dark:text-[#EAEAEA]"
+    {...storyblokEditable(blok)}
+  >
+    {blok.artikelgrid?.map((nestedBlok) => (
       <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
     ))}
   </div>
