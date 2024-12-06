e78b987d36c839d52128dda9eb28fb62b1d05682
MisterSaji <164487904+MisterSaji@users.noreply.github.com>
2024-12-06 11:12:03 +0100

    alle fonts en kleuren
    Alle fonts en kleuren zijn aangepast naar het juiste

--- components/Workshop/Workshop_blok.jsx
+++ components/Workshop/Workshop_blok.jsx
@@ -8,7 +8,7 @@ const Workshop_blok = ({ blok }) => (
     {...storyblokEditable(blok)}
   >
     <div className="w-[400px]">
-      <div className="flex justify-end mb-6 font-sans font-bold text-xl">
+      <div className="flex justify-end mb-6 font-sans font-bold text-xl text-donkerBlauw">
         <h1>{render(blok.Workshop_header1)}</h1>
       </div>
       <div className="flex justify-between">
@@ -20,22 +20,34 @@ const Workshop_blok = ({ blok }) => (
             <div className="font-font-sans font-bold text-xl">
               {render(blok.WorkshopNaam1)}
             </div>
-            <div>{render(blok.WorkshopSubtekst1)}</div>
-            <div className="flex justify-end">{render(blok.WorkshopTijd1)}</div>
+            <div className="font-sans font-normal">
+              {render(blok.WorkshopSubtekst1)}
+            </div>
+            <div className="flex justify-end font-montserrat font-normal">
+              {render(blok.WorkshopTijd1)}
+            </div>
           </div>
           <div className="bg-[#EBEEF3]  rounded p-2">
             <div className="font-sans font-bold text-xl">
               {render(blok.WorkshopNaam2)}
             </div>
-            <div>{render(blok.WorkshopSubtekst2)}</div>
-            <div className="flex justify-end">{render(blok.WorkshopTijd2)}</div>
+            <div className="font-sans font-normal">
+              {render(blok.WorkshopSubtekst2)}
+            </div>
+            <div className="flex justify-end font-montserrat font-normal">
+              {render(blok.WorkshopTijd2)}
+            </div>
           </div>
           <div className="bg-[#EBEEF3] p-2 rounded">
             <div className="font-sans font-bold text-xl">
               {render(blok.WorkshopNaam3)}
             </div>
-            <div>{render(blok.WorkshopSubtekst3)}</div>
-            <div className="flex justify-end">{render(blok.WorkshopTijd3)}</div>
+            <div className="font-sans font-normal">
+              {render(blok.WorkshopSubtekst3)}
+            </div>
+            <div className="flex justify-end font-montserrat font-normal">
+              {render(blok.WorkshopTijd3)}
+            </div>
           </div>
         </div>
       </div>
