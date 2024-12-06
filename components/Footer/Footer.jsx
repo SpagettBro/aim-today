f9b6ca2a1726c5f2e201187db673a62ccaea8247
Lucas <lucasbitalia@gmail.com>
2024-12-06 14:04:39 +0100

    footer en nav bar darkmode

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 
 const Footer = ({ blok }) => (
   <div
-    className="bg-gray-100 py-1 px-4 md:py-[20px]"
+    className="bg-gray-100 py-1 px-4 md:py-[20px] dark:bg-[#1E1E1E] dark:text-white" // Dark background and text color
     {...storyblokEditable(blok)}
   >
     {/* Main Content */}
@@ -10,117 +10,120 @@ const Footer = ({ blok }) => (
       {/* Links Section */}
       <div className="md:w-[322px]">
         {/* Mobile: Stacked Links in Flex Columns */}
-        <ul className="space-y-4 text-center md:hidden flex flex-wrap justify-center font-inter">
+        <ul className="space-y-4 text-center md:hidden flex flex-wrap justify-center font-inter dark:text-white">
           <li className="w-1/2 mt-2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Nieuws
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Interviews
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Artikels
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Evenementen
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Webinars
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Vacatures
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Contact
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Team
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Over
             </a>
           </li>
           <li className="w-1/2">
-            <a href="#" className="hover:text-oranje">
+            <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
               Nieuwsbrief
             </a>
           </li>
         </ul>
+
         {/* Desktop: Two Columns */}
-        <div className="hidden md:grid grid-cols-2 gap-x-8 gap-y-4 text-left">
+        <div className="hidden md:grid grid-cols-2 gap-x-8 gap-y-4 text-left dark:text-white">
           <ul className="space-y-4">
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Nieuws
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Interviews
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Artikels
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Evenementen
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Webinars
               </a>
             </li>
           </ul>
           <ul className="space-y-4">
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Vacatures
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Contact
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Team
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Over
               </a>
             </li>
             <li>
-              <a href="#" className="hover:text-oranje">
+              <a href="#" className="hover:text-oranje dark:hover:text-orange-500">
                 Nieuwsbrief
               </a>
             </li>
           </ul>
         </div>
+
         {/* Divider */}
-        <div className="h-[1px] bg-gray-300 hidden md:block w-[1200px] mt-[35px]" />
+        <div className="h-[1px] bg-gray-300 dark:bg-[#FFFFFF] hidden md:block w-[1200px] mt-[35px]" />
+
         {/* Logo */}
         <div className="mt-6 flex justify-center md:justify-start">
           <img
@@ -133,14 +136,14 @@ const Footer = ({ blok }) => (
 
       {/* Newsletter Section */}
       <div className="md:w-[430px] w-full flex flex-col items-center md:items-start mt-8 mr-[-700px] md:mt-0">
-        <p className="text-lg font-medium text-gray-700 mb-4 text-center md:text-left">
+        <p className="text-lg font-medium text-gray-700 mb-4 text-center md:text-left dark:text-white">
           Meld je aan voor de nieuwsbrief
         </p>
         <div className="flex w-full mb-6">
           <input
             type="email"
             placeholder="email@gmail.com"
-            className="flex-grow px-4 py-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-oranje"
+            className="flex-grow px-4 py-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-oranje dark:bg-[#1E1E1E] dark:text-white"
           />
           <button className="px-4 py-2 bg-oranje text-white rounded-r-lg hover:bg-orange-600 focus:outline-none">
             ⟶
@@ -149,19 +152,19 @@ const Footer = ({ blok }) => (
       </div>
 
       {/* Social Media Icons */}
-      <div className="md:mt-[230px] flex gap-6 justify-center md:justify-end w-full md:w-auto">
+      <div className="md:mt-[230px] flex gap-6 justify-center md:justify-end w-full md:w-auto dark:text-[#EAEAEA]">
         {[
           "instagram_icon",
           "facebook_icon",
           "youtube_icon",
           "linkedin_icon",
-          "x_icon",
+          "x_icon"
         ].map((icon, i) => (
           <img
             key={i}
             src={blok[icon].filename}
             alt={blok[icon].alt || icon}
-            className="h-8 w-8"
+            className="h-8 w-8 dark:text-[#EAEAEA]"
           />
         ))}
       </div>
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = ({ blok }) => {
 
   return (
     <header
-      className="bg-white w-full border-b border-gray-300 z-10 px-6"
+      className="bg-white w-full border-b border-gray-300 dark:border-[#121212] z-10 px-6 dark:bg-[#121212] dark:text-white"
       {...storyblokEditable(blok)}
     >
       <div className="flex items-center justify-between max-w-7xl mx-auto py-3">
@@ -37,28 +37,28 @@ const Navbar = ({ blok }) => {
           <nav className="hidden lg:flex space-x-6">
             <a
               href="/nieuws"
-              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm"
+              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm dark:text-white dark:hover:text-orange-500"
             >
               Nieuws
               <span className="block w-full h-[2px] bg-oranje opacity-0 group-hover:opacity-100 transition-all"></span>
             </a>
             <a
               href="/interviews"
-              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm"
+              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm dark:text-white dark:hover:text-orange-500"
             >
               Interviews
               <span className="block w-full h-[2px] bg-oranje opacity-0 group-hover:opacity-100 transition-all"></span>
             </a>
             <a
               href="/artikelen"
-              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm"
+              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm dark:text-white dark:hover:text-orange-500"
             >
               Artikelen
               <span className="block w-full h-[2px] bg-oranje opacity-0 group-hover:opacity-100 transition-all"></span>
             </a>
             <a
               href="/events"
-              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm"
+              className="text-[#2A3A4B] group hover:text-black transition duration-300 px-[10px] font-dm dark:text-white dark:hover:text-orange-500"
             >
               Evenementen
               <span className="block w-full h-[2px] bg-oranje opacity-0 group-hover:opacity-100 transition-all"></span>
@@ -66,7 +66,7 @@ const Navbar = ({ blok }) => {
           </nav>
 
           <div className="hidden lg:flex items-center space-x-4 ml-[225px]">
-            <button className="text-[#2A3A4B] hover:text-black transition duration-300 font-dm">
+            <button className="text-[#2A3A4B] hover:text-black transition duration-300 font-dm dark:text-white dark:hover:text-orange-500">
               Log In
             </button>
             <button className="text-white bg-donkerBlauw px-[15px] py-[10px] rounded-md font-dm">
@@ -76,30 +76,27 @@ const Navbar = ({ blok }) => {
         </div>
 
         <button
-          className="lg:hidden p-2 text-[#2A3A4B] focus:outline-none"
+          className="lg:hidden p-2 text-[#2A3A4B] focus:outline-none dark:text-white"
           onClick={toggleMenu}
         >
           <span
-            className={`block w-6 h-0.5 bg-gray-500 mb-1 transition-all duration-300 ${
-              menuOpen ? "transform rotate-45" : ""
-            }`}
+            className={`block w-6 h-0.5 bg-gray-500 mb-1 transition-all duration-300 ${menuOpen ? "transform rotate-45" : ""
+              }`}
           ></span>
           <span
-            className={`block w-6 h-0.5 bg-gray-500 mb-1 transition-all duration-300 ${
-              menuOpen ? "hidden" : ""
-            }`}
+            className={`block w-6 h-0.5 bg-gray-500 mb-1 transition-all duration-300 ${menuOpen ? "hidden" : ""
+              }`}
           ></span>
           <span
-            className={`block w-6 h-0.5 bg-gray-500 transition-all duration-300 ${
-              menuOpen ? "-rotate-45" : ""
-            }`}
+            className={`block w-6 h-0.5 bg-gray-500 transition-all duration-300 ${menuOpen ? "-rotate-45" : ""
+              }`}
           ></span>
         </button>
       </div>
 
       {menuOpen && (
-        <nav className="fixed inset-0 bg-gray-100 shadow-md z-50 overflow-y-auto">
-          <div className="flex items-center justify-between max-w-7xl mx-auto py-3 px-6 border-b border-gray-300">
+        <nav className="fixed inset-0 bg-gray-100 shadow-md z-50 overflow-y-auto dark:bg-[#121212]">
+          <div className="flex items-center justify-between max-w-7xl mx-auto py-3 px-6 border-b border-gray-300 dark:border-gray-700">
             <img
               src={blok.logo.filename}
               alt={blok.logo.alt || "News Logo"}
@@ -107,7 +104,7 @@ const Navbar = ({ blok }) => {
             />
             <button
               onClick={toggleMenu}
-              className="text-[#2A3A4B] hover:text-black transition duration-300 focus:outline-none"
+              className="text-[#2A3A4B] hover:text-black transition duration-300 focus:outline-none dark:text-white"
             >
               <span className="block w-6 h-0.5 bg-gray-500 mb-1 transform rotate-45"></span>
               <span className="block w-6 h-0.5 bg-gray-500 transform -rotate-45"></span>
@@ -117,25 +114,25 @@ const Navbar = ({ blok }) => {
           <div className="p-6 space-y-4">
             <a
               href="/nieuws"
-              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6"
+              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6 dark:text-white dark:hover:text-orange-500"
             >
               Nieuws
             </a>
             <a
               href="/interviews"
-              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6"
+              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6 dark:text-white dark:hover:text-orange-500"
             >
               Interviews
             </a>
             <a
               href="/artikelen"
-              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6"
+              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6 dark:text-white dark:hover:text-orange-500"
             >
               Artikelen
             </a>
             <a
               href="/evenementen"
-              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6"
+              className="block text-[#2a3a4b] text-2xl hover:text-oranje transition-all border-b border-gray-300 pb-6 dark:text-white dark:hover:text-orange-500"
             >
               Evenementen
             </a>
