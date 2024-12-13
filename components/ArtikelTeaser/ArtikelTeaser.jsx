0e49dbff19dc083bceaccd58c58634ca461fcb7a
Lucas <lucasbitalia@gmail.com>
2024-12-13 10:32:13 +0100

    blokken darkmode

--- components/ArtikelTeaser/ArtikelTeaser.jsx
+++ components/ArtikelTeaser/ArtikelTeaser.jsx
@@ -9,13 +9,13 @@ const ArtikelTeaser = ({ artikel }) => {
 
   return (
     <div
-      className="flex flex-col justify-between bg-white p-6"
+      className="flex flex-col justify-between bg-donkerOffWhite dark:bg-darkGrijs p-6 rounded-xl"
       {...storyblokEditable(artikel)}
     >
       {/* Afbeelding */}
       <div>
         <div className="relative mb-8">
-          <div className="absolute -bottom-2 -right-2 w-full h-full bg-oranje rounded-xl z-0"></div>
+          <div className="absolute -bottom-2 -right-2 w-full h-full bg-donkerOranje rounded-xl z-0"></div>
           <img
             className="relative object-cover object-center w-full h-48 rounded-xl z-10"
             src={artikel.afbeelding?.filename || ""}
@@ -24,12 +24,12 @@ const ArtikelTeaser = ({ artikel }) => {
         </div>
 
         {/* Titel */}
-        <h2 className="text-lg font-semibold font-dm leading-none text-donkerBlauw mb-4">
+        <h2 className="text-lg font-semibold font-dm leading-none text-donkerBlauw dark:text-donkerOffWhite mb-4">
           {artikel.titel || "Geen titel"}
         </h2>
 
         {/* Subtitel */}
-        <p className="text-sm leading-relaxed text-gray-500 mb-4 font-inter">
+        <p className="text-sm leading-relaxed text-lijnDonker dark:text-offWhite dark:text-opacity-80	 mb-4 font-inter">
           {artikel.subtitel || "Geen teaser beschikbaar"}
         </p>
       </div>
@@ -38,7 +38,7 @@ const ArtikelTeaser = ({ artikel }) => {
       <div className="mt-auto">
         <Link
           href={`/${artikel.slug}`}
-          className="inline-block mt-4 font-semibold font-montserrat text-oranje hover:text-donkerBlauw"
+          className="inline-block mt-4 font-semibold font-montserrat text-donkerOranje hover:text-donkerBlauw dark:hover:text-donkerOffWhite"
           title="Lees meer"
         >
           Lees meer »
