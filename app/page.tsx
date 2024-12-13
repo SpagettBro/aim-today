8ea4177094c573bf596a4b06aae3f9c948d8a0e9
SpagettBro <markgonzalez02@hotmail.com>
2024-12-13 11:54:34 +0100

    dfdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,24 @@
-import { Metadata } from 'next';
+import type { NextPage } from "next";
 import fetchData from "@utils/fetch-data";
 import StoryblokStory from "@storyblok/react/story";
 import { notFound } from "next/navigation";
-
-interface PageProps {
-  params: { slug?: string };
-}
-
-export default async function Page({ params }: PageProps) {
-  const slug = params.slug || 'home'; // Default to 'home' if no slug is provided
+ 
+type Props = {
+  params: { slug: string };
+};
+ 
+const Page: NextPage<Props> = async ({ params: { slug } }) => {
   const { data, status } = await fetchData(slug);
-
   if (status === 404) {
     return notFound();
   }
-
+ 
   return (
     <div>
       <StoryblokStory story={data.story} />
     </div>
   );
-}
-
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-  const slug = params.slug || 'home';
-  const { data } = await fetchData(slug);
-
-  return {
-    title: data.story.name,
-    // Add other metadata properties as needed
-  };
-}
-
+};
+ 
+export default Page;
+ 
